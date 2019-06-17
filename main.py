fe72c6114f0bab341f5701345363a5f5fca5c138
demo <demo@provisions-MacBook-Air.local>
2019-06-17 14:45:01 +0800

    modified

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import webapp2
 import jinja2
 import os
 from google.appengine.api import users
+from google.appengine.api import urlfetch
+import json
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -26,7 +28,7 @@ class NoUserHandler(webapp2.RequestHandler):
 #user is loggedin
 class LoggedInHandler(webapp2.RequestHandler):
     def get(self):
-        hometemplate = JINJA_ENVIRONMENT.get_template('templates/home.html')
+        hometemplate = JINJA_ENVIRONMENT.get_template('templates/homepage.html')
         user = users.get_current_user()
         email = user.nickname()
 
@@ -37,8 +39,25 @@ class LoggedInHandler(webapp2.RequestHandler):
 
 class SearchBarHandler(webapp2.RequestHandler):
     def get(self):
-        searchtemplate = JINJA_ENVIRONMENT.get_template('templates/search.html')
+        searchtemplate = JINJA_ENVIRONMENT.get_template('templates/tryanime.html')
         self.response.write(searchtemplate.render())
+    def post(self):
+        searchtemplate = JINJA_ENVIRONMENT.get_template('templates/tryanime1.html')
+        searchTerm=self.request.get('search')
+        endpoint_url='https://kitsu.io/api/edge/manga?page[limit]=20&filter[text]='+searchTerm
+        response = urlfetch.fetch(endpoint_url)
+        content = response.content
+        response_as_json = json.loads(content)
+        d={}
+        for i in range(len(response_as_json['data'])):
+            image_url=response_as_json['data'][i]['attributes']['posterImage']['medium']
+            titles=response_as_json['data'][i]['attributes']['canonicalTitle']
+            d[i]=[image_url,titles]
+        print(d)
+        dd = {'d': d}
+        self.response.write(searchtemplate.render(dd))
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainPageHandler),
