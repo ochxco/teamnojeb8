535db0a30efa2035292bb982fe85c69658fecfc8
demo <demo@provisions-MacBook-Air.local>
2019-06-17 14:29:17 +0800

    added Group class to models.py

--- models.py
+++ models.py
@@ -1,13 +1,21 @@
 from google.appengine.ext import ndb
 
-class Manga(ndb.model):
+class Manga(ndb.Model):
     manga_id = ndb.StringProperty(required=True)
     total_ratings = ndb.FloatProperty(required=False)
     average_ratings = ndb.FloatProperty(required=False)
     reccomendations_value = ndb.IntegerProperty(required=False)
     genre = ndb.StringProperty(required=False)
 
-class Friends(ndb.model):
-    user = ndb.StringProperty(required=True)
+class User(ndb.Model):
+    email = ndb.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    username = ndb.StringProperty(required=True)
     friends_list = ndb.StringProperty(required=False)
     groups = ndb.StringProperty(required=False)
+    user_recommendations = ndb.StringProperty(required=False)
+    user_ratings = ndb.StringProperty(required=False)
+
+class Group(ndb.Model):
+    chat = ndb.StringProperty(required=False)
+    members = ndb.KeyProperty(User)
