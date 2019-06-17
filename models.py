39d193ff59892b87571fbb1fa13a61596c7038d3
demo <demo@provisions-MacBook-Air.local>
2019-06-17 16:09:15 +0800

    models.py refining

--- models.py
+++ models.py
@@ -9,7 +9,6 @@ class Manga(ndb.Model):
 
 class User(ndb.Model):
     email = ndb.StringProperty(required=True)
-    name = ndb.StringProperty(required=True)
     friends_list = ndb.StringProperty(required=False)
     groups = ndb.StringProperty(required=False)
     user_recommendations = ndb.StringProperty(required=False)
