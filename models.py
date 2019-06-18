f5a1c7c41adf2159708a90dddbffe61edb532165
demo <demo@provisions-air.lan>
2019-06-18 21:46:20 +0800

    changed a bit of stuff for Manga object

--- models.py
+++ models.py
@@ -2,10 +2,12 @@ from google.appengine.ext import ndb
 
 class Manga(ndb.Model):
     manga_id = ndb.StringProperty(required=True)
-    total_ratings = ndb.FloatProperty(required=False)
+    manga_title = ndb.StringProperty(required=True)
+    total_ratings = ndb.JsonProperty(required=False)
     average_ratings = ndb.FloatProperty(required=False)
     reccomendations_value = ndb.IntegerProperty(required=False)
     genre = ndb.StringProperty(required=False)
+    reviews = ndb.JsonProperty(required=True)
 
 class MangaUser(ndb.Model):
     email = ndb.StringProperty(required=True)
@@ -13,8 +15,8 @@ class MangaUser(ndb.Model):
     friends_list = ndb.StringProperty(required=False)
     groups = ndb.StringProperty(required=False)
     user_recommendations = ndb.StringProperty(required=False)
-    user_ratings = ndb.JsonProperty(required=False)
-    user_reviews = ndb.JsonProperty(required=False)
+    user_ratings = ndb.JsonProperty(required=True)
+    user_reviews = ndb.JsonProperty(required=True)
 
 class Group(ndb.Model):
     chat = ndb.StringProperty(required=False)
